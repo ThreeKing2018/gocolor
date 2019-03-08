3fe7c1f3c04efd28a18c6cb83d425da2f94c48ce
百里 <sgfoot2020@gmail.com>
2019-03-08 14:41:38 +0800

    添加测试用例

--- README.md
+++ README.md
@@ -1,15 +1,26 @@
 # golang 命令行输出带颜色的字符串
 > 让你的命令行丰富多彩,输出不同颜色,自由组合
 
+
+# 安装
+```
+go get -u github.com/ThreeKing2018/gocolor
+```
+
+# 引用包
+```
+import "github.com/ThreeKing2018/gocolor"
+```
+
 # 基本色
 ```
-Green("绿色")
-White("白色")
-Yellow("黄色")
-Red("红色")
-Blue("蓝色")
-Magenta("洋红色")
-Cyan("蓝绿色")
+gocolor.Green("绿色")
+gocolor.White("白色")
+gocolor.Yellow("黄色")
+gocolor.Red("红色")
+gocolor.Blue("蓝色")
+gocolor.Magenta("洋红色")
+gocolor.Cyan("蓝绿色")
 ```
 
 ![](picture/1.jpg)
@@ -17,13 +28,13 @@ Cyan("蓝绿色")
 # 背景色
 
 ```
-GreenBG("绿色背景")
-WhiteBG("白色背景")
-YellowBG("黄色背景")
-RedBG("红色背景")
-BlueBG("蓝色背景")
-MagentaBG("洋红色背景")
-CyanBG("蓝绿色背景")
+gocolor.GreenBG("绿色背景")
+gocolor.WhiteBG("白色背景")
+gocolor.YellowBG("黄色背景")
+gocolor.RedBG("红色背景")
+gocolor.BlueBG("蓝色背景")
+gocolor.MagentaBG("洋红色背景")
+gocolor.CyanBG("蓝绿色背景")
 ```
 
 ![](picture/2.jpg)
@@ -31,10 +42,10 @@ CyanBG("蓝绿色背景")
 # 组合色
 
 ```
-BlueBG(SRed("红字蓝底色"))
-CyanBG(SYellow("黄字蓝绿底色"))
-MagentaBG(SRed("红字洋底色"))
-GreenBG(SMagentaBG("洋红字绿底"))
+gocolor.BlueBG(SRed("红字蓝底色"))
+gocolor.CyanBG(SYellow("黄字蓝绿底色"))
+gocolor.MagentaBG(SRed("红字洋底色"))
+gocolor.GreenBG(SMagentaBG("洋红字绿底"))
 ```
 
 ![](picture/1.jpg)
--- color_test.go
+++ color_test.go
@@ -4,14 +4,50 @@ import (
 	"testing"
 )
 
+var colorMap = map[int]string{
+	1:"green",
+}
+func colorFunc(color, s string) string {
+	result := ""
+	switch color {
+	case "green":
+		result = SGreen(s)
+	case "white":
+		result = SWhite(s)
+	case "yellow":
+		result = SYellow(s)
+	case "red":
+		result = SRed(s)
+	case "blue":
+		result = SBlue(s)
+	case "magenta":
+		result = SMagenta(s)
+	case "cyan":
+		result = SCyan(s)
+	}
+	return result
+}
+
 func TestColor(t *testing.T) {
-	Green("绿色")
-	White("白色")
-	Yellow("黄色")
-	Red("红色")
-	Blue("蓝色")
-	Magenta("洋红色")
-	Cyan("蓝绿色")
+	tests := []struct {
+		s string
+		color string
+		expected string
+	}{
+		{"g", "green", "\x1b[32mg\n\x1b[0m"},
+		{"w", "white", "\x1b[37mw\n\x1b[0m"},
+		{"y", "yellow","\x1b[33my\n\x1b[0m"},
+		{"r", "red",   "\x1b[31mr\n\x1b[0m"},
+		{"b", "blue",  "\x1b[34mb\n\x1b[0m"},
+		{"m", "magenta","\x1b[35mm\n\x1b[0m"},
+		{"c", "cyan",  "\x1b[36mc\n\x1b[0m"},
+	}
+	for _, row := range tests {
+		actual := colorFunc(row.color, row.s)
+		if actual != row.expected {
+			t.Errorf("color: %s, actual: %s, expected: %s", row.color, actual, row.expected)
+		}
+	}
 }
 
 func TestBackgroundColor(t *testing.T) {
